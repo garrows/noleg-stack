4016f1bc478196225027ab7eab12d48d41f473f3
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2014-01-19 21:22:36 +1000

    Removed escape slashes (from previous setup)

--- post-receive.sh
+++ post-receive.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -e
-REV=\`git rev-parse HEAD\`
-DIR=/var/www/\$REV/
-mkdir -p \$DIR
-echo "Checking out to \$DIR"
-GIT_WORK_TREE=\$DIR git checkout -f
+REV=`git rev-parse HEAD`
+DIR=/var/www/$REV/
+mkdir -p $DIR
+echo "Checking out to $DIR"
+GIT_WORK_TREE=$DIR git checkout -f
  
-cd \$DIR
+cd $DIR
 cd www
 npm install
 cd ../blog
@@ -15,13 +15,13 @@ npm install
 echo "Done installs"
  
 if [ -d /var/www/current ]; then
-  OLD_DIR=\`readlink /var/www/current\`
+  OLD_DIR=`readlink /var/www/current`
 fi
  
-echo "Linking \$DIR"
-ln -sfn \$DIR /var/www/current
+echo "Linking $DIR"
+ln -sfn $DIR /var/www/current
  
 if [ -d /var/www/current ]; then
-  echo "Removing old directory \$OLD_DIR"
-  rm -rf \$OLD_DIR
+  echo "Removing old directory $OLD_DIR"
+  rm -rf $OLD_DIR
 fi
