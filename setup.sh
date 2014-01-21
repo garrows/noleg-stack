fd3f828f3301117e5783b353d219aa02272c902f
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2014-01-21 17:01:11 +1000

    Fixed up mongo install

--- setup.sh
+++ setup.sh
@@ -14,12 +14,11 @@ sudo apt-get install -y git
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-10gen
-sudo service mongodb start
+sudo apt-get install -y mongodb-10gen
  
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get updateFasd
+sudo apt-get update
 sudo apt-get install -y nodejs
  
 sudo npm install -g forever express
