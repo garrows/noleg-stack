42ecf059766e75c1cc187d38c85ceb0bc634d5a6
Glen <glen.arrowsmith@gmail.com>
2014-05-09 13:52:18 +1000

    Fix node install bug on some systems

--- installSoftware.sh
+++ installSoftware.sh
@@ -15,7 +15,7 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 sudo apt-get update
 sudo apt-get install -y mongodb-10gen
 
-sudo apt-get install -y python-software-properties python g++ make
+sudo apt-get install -y software-properties-common python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
